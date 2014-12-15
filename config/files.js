f598dfb07e1310c23c550c8994ba44608daf8054
timsvoice <timsethvoice@gmail.com>
2014-12-15 15:03:56 -0500

    reordering js files

--- config/files.js
+++ config/files.js
@@ -18,12 +18,12 @@ module.exports = function(lineman) {
         "vendor/bower/jquery/dist/jquery.min.js",
         "vendor/bower/jquery.lazyload/jquery.lazyload.min.js",
         "vendor/js/angular.js",
+        "vendor/bower/angular-sanitize/angular-sanitize.min.js",
         "vendor/bower/showdown/src/showdown.js",
         "vendor/bower/angular-markdown-directive/markdown.js",
         "vendor/bower/foundation/js/foundation.js",
         "vendor/bower/foundation/js/foundation/foundation.topbar.js",
         "vendor/js/dropcap.min.js",
-        "vendor/bower/angular-sanitize/angular-sanitize.min.js",
         "vendor/js/**/*.js"
         
       ]
