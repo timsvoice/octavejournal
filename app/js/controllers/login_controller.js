fc60356c9215b4f827d340760c4857edde07358b
timsvoice <timsethvoice@gmail.com>
2014-12-16 15:06:43 -0500

    added language cookie

--- app/js/controllers/login_controller.js
+++ app/js/controllers/login_controller.js
@@ -3,7 +3,8 @@ app.controller('LoginController', function($scope, $location, AuthenticationServ
 
   function isLogged() {
     if ($cookieStore.get('isLogged') === 'true') {
-      $location.path('/en');
+      var lan = $cookieStore.get('lan');
+      $location.path(lan);
       console.log('success');
     }
   }
@@ -14,9 +15,11 @@ app.controller('LoginController', function($scope, $location, AuthenticationServ
     if($scope.user.language === "english" && $scope.user.password === "middleway" || $cookieStore.get('isLogged') === 'true'){
       $location.path('/en');
       $cookieStore.put('isLogged','true');
+      $cookieStore.put('lan','en');
     } else if ($scope.user.language === "chinese" && $scope.user.password === "middleway" || $cookieStore.get('isLogged') === 'true'){
       $location.path ('/cn');
       $cookieStore.put('isLogged','true');
+      $cookieStore.put('lan','cn');
     } else {
       alert("Your password is wrong or you didn't select a language. Please check your newsletter for your password");
        $cookieStore.remove('isLogged');
