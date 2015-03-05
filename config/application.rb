232d57a30ab9556c22bb73acfec2318c045d2192
timothy voice <timothyvoice@tm-l4.tsao-mckown.com>
2015-03-05 15:44:48 -0500

    add ckeditor precombile to config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ module Octavejournal
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     # config.i18n.available_locales = [:"zh-CN", :zh]
+    config.assets.precompile += Ckeditor.assets
   end
 end
