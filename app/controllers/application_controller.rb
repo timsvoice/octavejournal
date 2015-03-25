fcec44e24542a70c5d4323af008e40063ea21df4
timothy voice <timothyvoice@tm-110722.tsao-mckown.com>
2015-03-25 18:48:50 -0400

    scope locale bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   private
 
     def set_locale
-      I18n.locale = params[:locale] || I18n.default_locale
+      I18n.locale = params[:locale]
       Rails.application.routes.default_url_options[:locale]= I18n.locale    
     end
   
