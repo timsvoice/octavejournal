28beda2d30aec83cf7729f19b022891680e7b67c
timothy voice <timothyvoice@timothys-MacBook-Pro.local>
2015-03-29 21:12:19 -0400

    removed 's' from rubygems

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :ssh_options, { :forward_agent => true }
 # Allow for password requests to surface in local terminal
 
 # Credentials
-set :user, "tim"
+set :user, "root"
 
 set :deploy_to, "/home/rails/octavejournal"
 set :use_sudo, false
